fd66444a3d77d1e869fc9329de9fccab2d60d264
qk0611 <160273317+qk0611@users.noreply.github.com>
2024-05-14 12:54:34 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+      //console.log("Hi JS");
+      //document.write("Hi JS");
+      //console.info("Hi JS");
+      //console.error("Hi JS");
+      console.warn("Hi JS");
---
+++ Базовое программирование/a.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() 
+{
+    printf("Helloqoekjf");
+    return 0;
+}
